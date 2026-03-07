03ec0f2ee0f4f39973661c1d85a21be7b3cde0b7
Shane Williams <shane.williams@resourcekraft.com>
2026-03-07 14:57:24 +0000

    feat: pwd command

---
+++ internal/shell/commands/pwd.go
@@ -0,0 +1,15 @@
+package commands
+
+import "github.com/shanewilliams/shell-quest/internal/shell"
+
+type Pwd struct{}
+
+func NewPwd() *Pwd { return &Pwd{} }
+
+func (p *Pwd) Name() string      { return "pwd" }
+func (p *Pwd) Aliases() []string { return nil }
+func (p *Pwd) Tier() string      { return "beginner" }
+
+func (p *Pwd) Run(args []string, cwd string, fs *shell.FS) shell.Result {
+	return shell.Result{Output: cwd}
+}
